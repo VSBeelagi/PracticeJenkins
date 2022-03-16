13b06b3ed39cd8d182bae5599698103746b5591e
Vijayalaxmi <Vijayalaxmi@DESKTOP-EUNFVH5>
2022-03-16 11:04:45 +0530

    ReadData updated

---
+++ src/test/java/Org/ReadDataTest.java
@@ -0,0 +1,18 @@
+package Org;
+
+import org.testng.annotations.Test;
+
+public class ReadDataTest {
+	
+	@Test
+	
+	public void readData()
+	{
+		String BROWSER= System.getProperty("browser");
+		System.out.println(BROWSER);
+		
+		String URL= System.getProperty("url");
+		System.out.println(URL);
+	}
+
+}
